4501de1c7c5bdd213f0ffe58ed14bde946bbd329
vim <9wimu9@gmail.com>
2022-11-05 00:25:18 +0530

    refactor MemberTest.php

--- eyepax/tests/Feature/MemberTest.php
+++ eyepax/tests/Feature/MemberTest.php
@@ -11,7 +11,7 @@ class MemberTest extends TestCase
 {
     use RefreshDatabase;
 
-    public $memberData = [
+    public array $memberData = [
         'full_name' => 'Andrew Smith',
         'email' => 'andrew@expo.com',
         'telephone' => '0655147147',
@@ -20,68 +20,70 @@ class MemberTest extends TestCase
         'comments' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     ];
 
+    public string $indexUri = '/members';
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_member_screen_can_be_rendered()
+    public function test_member_screen_can_be_rendered(): void
     {
-        $response = $this->get('/members');
+        $response = $this->get($this->indexUri);
 
         $response->assertStatus(200);
     }
 
-    public function test_new_member_can_add_to_the_team()
+    public function test_new_member_can_add_to_the_team(): void
     {
         $response = $this->post('/members', $this->memberData);
-        $response->assertRedirect('/members');
+        $response->assertRedirect($this->indexUri);
     }
 
-    public function test_member_registration_requires_unique_email()
+    public function test_member_registration_requires_unique_email(): void
     {
         Member::factory()->create(['email' => 'other@email.com']);
         $response = $this->post('/members', $this->memberData);
         $response->assertValid('email');
-        $response->assertRedirect('/members');
+        $response->assertRedirect($this->indexUri);
     }
-
-    public function test_member_registration_requires_unique_telephone()
+//
+    public function test_member_registration_requires_unique_telephone(): void
     {
         Member::factory()->create(['telephone' => '11']);
         $response = $this->post('/members', $this->memberData);
         $response->assertValid('telephone');
-        $response->assertRedirect('/members');
+        $response->assertRedirect($this->indexUri);
     }
 
-    public function test_member_registration_requires_full_name()
+    public function test_member_registration_requires_full_name(): void
     {
         $response = $this->post('/members', $this->memberData);
         $response->assertValid('full_name');
-        $response->assertRedirect('/members');
+        $response->assertRedirect($this->indexUri);
     }
 
-    public function test_member_registration_requires_joined_date()
+    public function test_member_registration_requires_joined_date(): void
     {
         $response = $this->post('/members', $this->memberData);
         $response->assertValid('joined_date');
-        $response->assertRedirect('/members');
+        $response->assertRedirect($this->indexUri);
     }
 
-    public function test_member_registration_requires_formatted_joined_date()
+    public function test_member_registration_requires_formatted_joined_date(): void
     {
         $memberData = $this->memberData;
-//        $memberData['joined_date']='sdsd';
+        $memberData['joined_date']='2022-12-11';
         $response = $this->post('/members', $memberData);
         $response->assertValid('joined_date');
-        $response->assertRedirect('/members');
+        $response->assertRedirect($this->indexUri);
     }
 
-    public function test_member_registration_requires_current_route()
+    public function test_member_registration_requires_current_route(): void
     {
         $response = $this->post('/members', $this->memberData);
         $response->assertValid('current_route');
-        $response->assertRedirect('/members');
+        $response->assertRedirect($this->indexUri);
     }
 
 }
