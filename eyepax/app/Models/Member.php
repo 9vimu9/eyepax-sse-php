bfdf844ee23f2913c54c6c0baab9229aaa0378dc
vim <9wimu9@gmail.com>
2022-11-07 12:02:21 +0530

    update repo with laravel data

--- eyepax/app/Models/Member.php
+++ eyepax/app/Models/Member.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
+use App\DTOs\MemberDTO;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\LaravelData\WithData;
 
 class Member extends Model
 {
     use HasFactory;
+    use WithData;
+
+    protected $dataClass = MemberDTO::class;
 
     protected $fillable = [
         'full_name', 'email', 'telephone', 'joined_date', 'current_route', 'comments'
--- eyepax/app/Repositories/member/MySqlMemberRepository.php
+++ eyepax/app/Repositories/member/MySqlMemberRepository.php
@@ -18,46 +18,28 @@ class MySqlMemberRepository implements MemberRepositoryInterface
 
     public function store(Request $request): MemberDTO
     {
-        $member = $this->builder->create([
+        return $this->builder->create([
             'full_name' => $request->get('full_name'),
             'email' => $request->get('email'),
             'telephone' => $request->get('telephone'),
             'joined_date' => $request->get('joined_date'),
             'current_route' => $request->get('current_route'),
             'comments' => $request->get('comments'),
-        ]);
+        ])->getData();
 
-        return new MemberDTO(
-            $member->id,
-            $member->full_name,
-            $member->email,
-            $member->telephone,
-            $member->joined_date,
-            $member->current_route,
-            $member->comments
-        );
     }
 
     public function update(Request $request, int $memberID): MemberDTO
     {
-        $member = tap($this->builder->findOrFail($memberID))->update([
+        return tap($this->builder->findOrFail($memberID))->update([
             'full_name' => $request->get('full_name'),
             'email' => $request->get('email'),
             'telephone' => $request->get('telephone'),
             'joined_date' => $request->get('joined_date'),
             'current_route' => $request->get('current_route'),
             'comments' => $request->get('comments'),
-        ])->first();
+        ])->first()->getData();
 
-        return new MemberDTO(
-            $member->id,
-            $member->full_name,
-            $member->email,
-            $member->telephone,
-            $member->joined_date,
-            $member->current_route,
-            $member->comments
-        );
     }
 
     public function delete(int $memberID): bool
