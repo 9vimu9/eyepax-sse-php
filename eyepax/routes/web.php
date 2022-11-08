0cd10b6cbfa9f9c424b5c7c0092370478a0a0f24
vim <9wimu9@gmail.com>
2022-11-08 10:42:31 +0530

    breeeze

--- eyepax/routes/web.php
+++ eyepax/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\MemberController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::controller(MemberController::class)->prefix("/members")->group(function () {
-    Route::get('', 'index')->name("members.index");
-    Route::post('', 'store')->name("members.store");
-    Route::put('/{id}', 'update')->name("members.update");
-    Route::delete('/{id}', 'destroy')->name("members.delete");
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
